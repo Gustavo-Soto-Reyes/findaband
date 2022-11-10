de104dc01e90d151594c449128a0f8914ac6a4e6
Gustavo-Soto-Reyes <83844074+Gustavo-Soto-Reyes@users.noreply.github.com>
2022-11-10 13:18:38 -0500

    add required pages

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,53 @@
-import { StyleSheet } from 'react-native';
+// import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+// import EditScreenInfo from "../components/EditScreenInfo";
+// import { Text, View } from "../components/Themed";
+// import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+// export default function TabOneScreen({
+//   navigation,
+// }: RootTabScreenProps<"TabOne">) {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Band Finder</Text>
+//       <View
+//         style={styles.separator}
+//         lightColor="#eee"
+//         darkColor="rgba(255,255,255,0.1)"
+//       />
+//       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: "80%",
+//   },
+// });
+
+import React from "react";
+import { Text, View } from "react-native";
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default HomeScreen;
